fcd9e7b28723b8fae7624ce3d98ad20997b90918
kol4egraf0-0 <genitya408@gmail.com>
2025-06-08 00:15:05 +0300

    add listView

--- presentation/src/main/java/com/example/presentation/fragment/profile/ProfileFragment.kt
+++ presentation/src/main/java/com/example/presentation/fragment/profile/ProfileFragment.kt
@@ -4,16 +4,33 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import com.example.presentation.R
+import com.example.presentation.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
+
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+    ): View {
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val menuItems = arrayOf("Написать в поддержку", "Настройки", "Выйти из аккаунта")
+
+        val adapter = object : ArrayAdapter<String>(requireContext(), R.layout.item_profile, R.id.itemText,
+            menuItems
+        ) {}
+
+        binding.profileListView.adapter = adapter
     }
 }
