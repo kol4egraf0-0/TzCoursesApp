461f2404173fd10427ec2680263d17c3cd90d664
kol4egraf0-0 <genitya408@gmail.com>
2025-06-07 17:29:04 +0300

    update ui logic

--- presentation/src/main/java/com/example/presentation/fragment/main/MainFragment.kt
+++ presentation/src/main/java/com/example/presentation/fragment/main/MainFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -17,6 +18,8 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
 class MainFragment : Fragment() {
     private val viewModel: MainViewModel by viewModel()
     private lateinit var adapter: CourseAdapter
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -32,9 +35,13 @@ class MainFragment : Fragment() {
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
 
+        view.findViewById<TextView>(R.id.sortButton).setOnClickListener {
+            adapter.sortByDate()
+        }
+
         viewLifecycleOwner.lifecycleScope.launch {
             viewModel.courses.collect { adapter.updateData(it) }
         }
-    }
 
+    }
 }
--- presentation/src/main/java/com/example/presentation/fragment/main/adapter/CourseAdapter.kt
+++ presentation/src/main/java/com/example/presentation/fragment/main/adapter/CourseAdapter.kt
@@ -1,31 +1,51 @@
 package com.example.presentation.fragment.main.adapter
 
-import android.text.Layout
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.RatingBar
 import android.widget.TextView
-import androidx.recyclerview.widget.ListAdapter
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.domain.viewmodels.entity.Course
 import com.example.presentation.R
 
 class CourseAdapter(private var items: List<Course>) : RecyclerView.Adapter<CourseAdapter.CourseViewHolder>() {
 
+    private var isSortedByDate = false
+
     inner class CourseViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         private val title: TextView = view.findViewById(R.id.titleCourse)
         private val text: TextView = view.findViewById(R.id.textCourse)
         private val price: TextView = view.findViewById(R.id.priceCourse)
-        private val rating: RatingBar = view.findViewById(R.id.ratingBar)
+        private val rating: TextView = view.findViewById(R.id.textRate)
         private val startDate: TextView = view.findViewById(R.id.dateStartCourse)
+        private val imageView: ImageView = view.findViewById(R.id.courseImage)
+        private val bookmarkIcon : ImageButton = view.findViewById(R.id.bookmarkIcon)
 
         fun bind(course: Course) {
             title.text = course.title
             text.text = course.text
             price.text = "${course.price}₽"
-            rating.rating = course.rate.toFloat()
+            rating.text = course.rate.toString()
             startDate.text = course.startDate
+
+            Glide.with(itemView.context)
+                .load(android.R.drawable.ic_delete)
+                .into(imageView)
+
+
+            bookmarkIcon.setOnClickListener {
+                bookmarkIcon.isSelected = !bookmarkIcon.isSelected
+                if (bookmarkIcon.isSelected) {
+                    bookmarkIcon.setColorFilter(ContextCompat.getColor(itemView.context, R.color.green))
+                } else {
+                    bookmarkIcon.setColorFilter(ContextCompat.getColor(itemView.context, R.color.white))
+                }
+            }
         }
     }
 
@@ -42,6 +62,16 @@ class CourseAdapter(private var items: List<Course>) : RecyclerView.Adapter<Cour
 
     fun updateData(newCourses: List<Course>) {
         items = newCourses
+        notifyDataSetChanged()
+    }
+
+    fun sortByDate() {
+        isSortedByDate = !isSortedByDate
+        items = if (isSortedByDate) {
+            items.sortedBy { it.startDate }
+        } else {
+            items.shuffled()
+        }
         notifyDataSetChanged()
     }
 }
